341d2ba4b71fe7ff947c5b07ea374557b6f287df
sengonik <44993324+sengonik@users.noreply.github.com>
2022-12-31 21:21:59 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
 function addFile(id, filename) {
     if (id === 0) {
-        document.querySelector('#nofile').innerHTML = 'Merges...' ;
+        document.querySelector('#nofile').innerHTML = '';
         /*function show(){if (document.getElementById)document.getElementById("nofile").style.visibility = "visible";}
         function hide(){if (document.getElementById)document.getElementById("nofile").style.visibility = "hidden";} // toggle "on" and "off" states every 450 ms to achieve a blink effect
         // end after 4500 ms (less than five seconds)
