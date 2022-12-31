5a3058f013a5049cf8312b6c45f34b5466930eb7
sengonik <44993324+sengonik@users.noreply.github.com>
2022-12-31 11:50:45 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,19 +62,6 @@ function addFile(id, filename) {
     `);
 }
 
-function uploadFiles() {
-        var files = document.getElementById('file_upload').files;
-        if(files.length==0){
-          alert("Please first choose or drop any file(s)...");
-          return;
-        }
-        var filenames="";
-        for(var i=0;i<files.length;i++){
-          filenames+=files[i].name+"\n";
-        }
-        alert("Selected file(s) :\n____________________\n"+filenames);
-}
-
 function removeFile(event) {
     const id = parseInt(event.currentTarget.parentNode.id.slice(4));
     pdfFiles.splice(id, 1);
